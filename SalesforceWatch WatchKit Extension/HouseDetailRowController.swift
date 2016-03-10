2397a3db13242d95a187a479af96aecb3c166fd2
Quinton Wall <qwall@salesforce.com>
2016-03-10 10:16:25 -0800

    added new properties screen

---
+++ SalesforceWatch WatchKit Extension/HouseDetailRowController.swift
@@ -0,0 +1,9 @@
+//
+//  HouseDetailRowController.swift
+//  LightningRealtor
+//
+//  Created by Quinton Wall on 3/10/16.
+//  Copyright © 2016 Salesforce, Inc. All rights reserved.
+//
+
+import Foundation
---
+++ SalesforceWatch WatchKit Extension/HouseListInterfaceController.swift
@@ -0,0 +1,9 @@
+//
+//  HouseListInterfaceController.swift
+//  LightningRealtor
+//
+//  Created by Quinton Wall on 3/10/16.
+//  Copyright © 2016 Salesforce, Inc. All rights reserved.
+//
+
+import Foundation
